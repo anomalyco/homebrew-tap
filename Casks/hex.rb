b6fe1fd5d752a133067e7921977efe3dc91dda7a
mabraithwaite <14130304+mabraithwaite@users.noreply.github.com>
2026-09-05 18:40:40 -0400

    fix(hex): remove deprecated verified parameter

--- Casks/hex.rb
+++ Casks/hex.rb
@@ -2,8 +2,7 @@ cask "hex" do
   version "2.1.16"
   sha256 "102dc1a8d80dc359ee15a30141957b6a311e581214b44a3c615026fa91288b8a"
 
-  url "https://pub-089d681d41754031a4aefa7017d8c2fb.r2.dev/releases/HEX-#{version}-arm64.dmg",
-      verified: "pub-089d681d41754031a4aefa7017d8c2fb.r2.dev/"
+  url "https://pub-089d681d41754031a4aefa7017d8c2fb.r2.dev/releases/HEX-#{version}-arm64.dmg"
   name "Hex"
   desc "Local-first voice dictation"
   homepage "https://hex.kitlangton.dev/"
